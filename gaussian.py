b154f85feb37cca5be97e83d2b006d8fb9fe180e
Bogdan88887 <fchat8420@gmail.com>
2024-10-16 10:20:19 +0000

    gaussian.py

--- gaussian.py
+++ gaussian.py
@@ -1,42 +1,42 @@
 import numpy as np
-from multiprocessing import Pool, cpu_count
 
-# Функция для параллельного вычитания строк
-def eliminate_row(args):
-    Ab, i, k = args
-    factor = Ab[k, i] / Ab[i, i]
-    Ab[k, i:] -= factor * Ab[i, i:]
-
-# Прямой ход метода Гаусса
 def gaussian_elimination(A, b):
     n = len(b)
-    Ab = np.hstack([A, b.reshape(-1, 1)])  # расширенная матрица
+    # Объединяем матрицу A и вектор b в расширенную матрицу
+    Ab = np.hstack([A, b.reshape(-1, 1)])
 
+    # Прямой ход
     for i in range(n):
-        # Нахождение ведущего элемента для текущего столбца
-        max_row = np.argmax(np.abs(Ab[i:, i])) + i
-        Ab[[i, max_row]] = Ab[[max_row, i]]  # Перестановка строк
+        # Нормализуем строку с максимальным элементом
+        max_row_index = np.argmax(np.abs(Ab[i:, i])) + i
+        Ab[[i, max_row_index]] = Ab[[max_row_index, i]]
 
-        # Параллельное выполнение вычитания строк
-        with Pool(cpu_count()) as p:
-            p.map(eliminate_row, [(Ab, i, k) for k in range(i + 1, n)])
+        # Приведение к треугольной форме
+        for j in range(i + 1, n):
+            factor = Ab[j, i] / Ab[i, i]
+            Ab[j, i:] -= factor * Ab[i, i:]
 
-    # Обратный ход для нахождения решений
+    # Обратный ход
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
-        x[i] = (Ab[i, -1] - np.dot(Ab[i, i+1:n], x[i+1:n])) / Ab[i, i]
+        x[i] = Ab[i, -1]  # Начинаем с последнего столбца
+        for j in range(i + 1, n):
+            x[i] -= Ab[i, j] * x[j]
+        x[i] /= Ab[i, i]
 
     return x
 
-# Пример использования
-if __name__ == '__main__':
-    # Пример матрицы A и вектора b
-    A = np.array([[3.0, 2.0, -4.0],
-                  [2.0, 3.0, 3.0],
-                  [5.0, -3.0, 1.0]])
+# Задаем матрицу A и вектор b
+A = np.array([
+    [1.0, 2.0, 3.0],
+    [0.0, 1.0, 4.0],
+    [5.0, 6.0, 0.0]
+])
 
-    b = np.array([3.0, 15.0, 14.0])
+b = np.array([14.0, 10.0, 32.0])
 
-    # Решение СЛАУ методом Гаусса
-    x = gaussian_elimination(A, b)
-    print("Решение СЛАУ:", x)
+# Решение системы
+solution = gaussian_elimination(A, b)
+
+# Выводим результат
+print("Решение СЛАУ:", np.round(solution, decimals=6))
